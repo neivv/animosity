67282f58597df20147c340a0a484b196a4a128d2
Markus Heikkinen <ittevien@gmail.com>
2020-12-13 21:37:55 +0200

    Optimize grp decode (It's not a hot part, just for fun)
    And fix a bug where it broke if grp width/height weren't equal

--- src/grp_decode.rs
+++ src/grp_decode.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use std::fmt;
 
 use byteorder::{ByteOrder, LittleEndian};
@@ -69,52 +70,58 @@ fn parse_header(grp: &[u8], frame: u16) -> Result<GrpHeader<'_>, Error> {
 }
 
 impl<'a> GrpHeader<'a> {
-    /// callback is x, y, color
+    /// callback is (pixel offset in width * height buffer), color
     /// x/y are relative to frame topleft, not sprite.
-    fn decode<F: FnMut(usize, usize, u8)>(&self, mut set_color: F) -> Result<(), Error> {
+    fn decode<F: FnMut(usize, u8)>(&self, mut set_color: F) -> Result<(), Error> {
         let width = self.frame_width as usize;
+        let mut line_start = self.x as usize + self.y as usize * self.width as usize;
         for y in 0..(self.frame_height as usize) {
-            let line_offset = self.frame_lines.get((y * 2)..)
+            let line_offset = self.frame_lines.get(y.wrapping_mul(2)..)
+                .and_then(|x| x.get(..2))
                 .map(|x| LittleEndian::read_u16(x))
                 .ok_or_else(|| Error)?;
             let mut line_data = self.frame_lines.get((line_offset as usize)..)
                 .ok_or_else(|| Error)?;
             let mut x = 0;
             while x < width {
+                let max_width = width - x;
                 let (&byte, rest) = line_data.split_first()
                     .ok_or_else(|| Error)?;
                 if byte & 0x80 != 0 {
                     // Transparent
-                    x += (byte & 0x7f) as usize;
+                    x = x.wrapping_add((byte & 0x7f) as usize);
                     line_data = rest;
                 } else if byte & 0x40 != 0 {
                     // memset
                     let length = (byte & 0x3f) as usize;
-                    let length = (width - x).min(length);
-                    let end = x + length;
+                    let length = max_width.min(length);
                     let (&color, rest) = rest.split_first()
                         .ok_or_else(|| Error)?;
-                    for x in x..end {
-                        set_color(x, y, color);
+                    let start = line_start.wrapping_add(x);
+                    let end = start.wrapping_add(length);
+                    for pos in start..end {
+                        set_color(pos, color);
                     }
-                    x = end;
+                    x = x.wrapping_add(length);
                     line_data = rest;
                 } else {
                     // memcpy
                     let length = (byte & 0x3f) as usize;
-                    let length = (width - x).min(length);
-                    let end = x + length;
+                    let length = max_width.min(length);
                     let data = rest.get(..length)
                         .ok_or_else(|| Error)?;
-                    for (x, color) in (x..end).zip(data.iter().copied()) {
-                        set_color(x, y, color);
+                    let start = line_start.wrapping_add(x);
+                    let end = start.wrapping_add(length);
+                    for (pos, color) in (start..end).zip(data.iter().copied()) {
+                        set_color(pos, color);
                     }
 
-                    x = end;
+                    x = x.wrapping_add(length);
                     line_data = rest.get(length..)
                         .ok_or_else(|| Error)?;
                 }
             }
+            line_start = line_start.wrapping_add(self.width as usize);
         }
         Ok(())
     }
@@ -131,20 +138,16 @@ pub fn frame_count(grp: &[u8]) -> Result<u16, Error> {
 /// This differentiates between transparent 0/0/0/0 and whatever palette[0] is.
 /// (Usually 0/0/0/255)
 pub fn decode_grp_to_rgba(grp: &[u8], frame: u16, palette: &[u8]) -> Result<GrpFrame, Error> {
-    if palette.len() != 0x400 {
-        return Err(Error);
-    }
+    let palette: &[u8; 0x400] = palette.try_into().map_err(|_| Error)?;
 
     let header = parse_header(grp, frame)?;
     let mut buffer = vec![0; header.width as usize * header.height as usize * 4];
-    let start_pos = header.x as usize + header.y as usize * header.height as usize;
-    header.decode(|x, y, color| {
-        let pos = start_pos
-            .wrapping_add(x)
-            .wrapping_add(y.wrapping_mul(header.height as usize))
-            .wrapping_mul(4);
-        let color = &palette[(color as usize * 4)..][..4];
-        (&mut buffer[pos..(pos.wrapping_add(4))]).copy_from_slice(color);
+    header.decode(|pos, color| {
+        let pos = pos.wrapping_mul(4);
+        if let Some(out) = buffer.get_mut(pos..(pos.wrapping_add(4))) {
+            let color = &palette[(color as usize * 4)..][..4];
+            out.copy_from_slice(color);
+        }
     })?;
     Ok(GrpFrame {
         data: buffer,
@@ -166,32 +169,31 @@ pub fn decode_grp_to_rgba_and_teamcolor(
     frame: u16,
     palette: &[u8],
 ) -> Result<(GrpFrame, Vec<u8>), Error> {
-    if palette.len() != 0x400 {
-        return Err(Error);
-    }
+    let palette: &[u8; 0x400] = palette.try_into().map_err(|_| Error)?;
 
     let header = parse_header(grp, frame)?;
     let mut buffer = vec![0; header.width as usize * header.height as usize * 4];
     let mut teamcolor = vec![0; header.width as usize * header.height as usize * 4];
-    let start_pos = header.x as usize + header.y as usize * header.height as usize;
-    header.decode(|x, y, color| {
-        let pos = start_pos
-            .wrapping_add(x)
-            .wrapping_add(y.wrapping_mul(header.height as usize))
-            .wrapping_mul(4);
-        if color >= 8 && color < 0x10 {
-            // scale to 1.0, 8/9, 7/9, ..., 1/9 (No 0.0)
-            let strength = (((1.0 - ((color - 8) as f32) / 9.0)) * 255.0) as u8;
-            for i in 0..3 {
-                buffer[pos + i] = strength;
-                teamcolor[pos + i] = 255;
+    header.decode(|pos, color| {
+        let pos = pos.wrapping_mul(4);
+        if let Some(out) = buffer.get_mut(pos..(pos.wrapping_add(4))) {
+            if color >= 8 && color < 0x10 {
+                // scale to 1.0, 8/9, 7/9, ..., 1/9 (No 0.0)
+                let strength = (((1.0 - ((color.wrapping_sub(8)) as f32) / 9.0)) * 255.0) as u8;
+                for i in 0..3 {
+                    out[i] = strength;
+                }
+                out[3] = 255;
+                if let Some(tc) = teamcolor.get_mut(pos..(pos.wrapping_add(4))) {
+                    for i in 0..4 {
+                        tc[i] = 255;
+                    }
+                }
+            } else {
+                let color = &palette[(color as usize * 4)..][..4];
+                out.copy_from_slice(color);
             }
-            buffer[pos + 3] = 255;
-            teamcolor[pos + 3] = 255;
-        } else {
-            let color = &palette[(color as usize * 4)..][..4];
-            (&mut buffer[pos..(pos.wrapping_add(4))]).copy_from_slice(color);
-        };
+        }
     })?;
     Ok((GrpFrame {
         data: buffer,
@@ -207,12 +209,10 @@ pub fn decode_grp_to_rgba_and_teamcolor(
 pub fn decode_grp_to_paletted(grp: &[u8], frame: u16) -> Result<GrpFrame, Error> {
     let header = parse_header(grp, frame)?;
     let mut buffer = vec![0; header.width as usize * header.height as usize];
-    let start_pos = header.x as usize + header.y as usize * header.height as usize;
-    header.decode(|x, y, color| {
-        let pos = start_pos
-            .wrapping_add(x)
-            .wrapping_add(y.wrapping_mul(header.height as usize));
-        buffer[pos] = color;
+    header.decode(|pos, color| {
+        if let Some(out) = buffer.get_mut(pos) {
+            *out = color;
+        }
     })?;
     Ok(GrpFrame {
         data: buffer,
